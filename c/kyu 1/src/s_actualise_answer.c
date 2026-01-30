c2de728fb35503aa6c31e0c36887b3776dee5fef
Sung Jin-Woo <louis.cagnion.pro@gmail.com>
2026-01-31 00:09:02 +0100

    j'y suis presque

--- c/kyu 1/src/s_actualise_answer.c
+++ c/kyu 1/src/s_actualise_answer.c
@@ -12,8 +12,6 @@
  */
 void	set_valid_pos(int nb, int line, int col, int ***available_nbs, int **solution)
 {
-	if (!available_nbs[nb][line][col])
-		return ;
 	//put the number in the solution board
 	solution[line][col] = nb + 1;
 	//set every other box on the same column to zero
